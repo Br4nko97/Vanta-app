cb12d98d74ceec4964d405186fa40649a52b032b
Br4nko97 <giomailsfx@gmail.com>
2026-05-04 16:34:20 +0200

    Migrate KSP to KAPT

--- vanta/app/build.gradle.kts
+++ vanta/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp")
+    id("kotlin-kapt")
 }
 
 android {
@@ -62,7 +62,7 @@ dependencies {
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
-    ksp("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
 
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
